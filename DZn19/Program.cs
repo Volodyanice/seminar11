e862712b5cab9f0616c6137cb902a2d479a8a5de
Volodya <skochilovvladimir@yandex.ru>
2023-03-11 03:34:23 +0700

    n19

---
+++ DZn19/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+int len = number.Length;
+if (len == 5)
+{
+    if(number[0] == number [4] && number[1] == number[3])
+    {
+        System.Console.WriteLine("палиндром");
+    }
+    else
+    {
+        System.Console.WriteLine("не палиндром");
+    }
+}
+else
+{
+    System.Console.WriteLine("число не пятизначное");
+}
