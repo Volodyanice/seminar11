e9de805af05809207f7ed6719bb811d6f75b3057
Volodya <skochilovvladimir@yandex.ru>
2023-03-11 03:45:38 +0700

    n21

---
+++ DZn21/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("x1: y1: z1:");
+double x1 = Convert.ToDouble(Console.ReadLine());
+double y1 = Convert.ToDouble(Console.ReadLine());
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("x2: y2: z2:");
+double x2 = Convert.ToDouble(Console.ReadLine());
+double y2 = Convert.ToDouble(Console.ReadLine());
+double z2 = Convert.ToDouble(Console.ReadLine());
+double coor1_coor2 = System.Math.Sqrt((System.Math.Pow((x2 - x1), 2)) + System.Math.Pow((y2 - y1), 2) + System.Math.Pow((z2-z1), 2));
+System.Console.WriteLine("{0: 0.00}", coor1_coor2);
